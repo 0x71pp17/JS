50f32dd218423e70bd547c8d13fd5ed16fd9101b
0x71pp17 <tippitj@gmail.com>
2025-11-05 22:04:48 -0500

    Update script.js

--- Modular/Calculator/script.js
+++ Modular/Calculator/script.js
@@ -1,16 +1,20 @@
+// Display input or operator on screen
 function display(value) {
   document.getElementById('result').value += value;
 }
 
+// Clear entire display
 function clearScreen() {
   document.getElementById('result').value = '';
 }
 
+// Remove last character
 function backspace() {
   let currentValue = document.getElementById('result').value;
   document.getElementById('result').value = currentValue.slice(0, -1);
 }
 
+// Calculate result using eval()
 function calculate() {
   let expression = document.getElementById('result').value;
   try {
